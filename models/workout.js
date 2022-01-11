00d9415e9daacdea0a5375f9744c0c06bc397d34
TeBossIzHere <72513020+TeBossIzHere@users.noreply.github.com>
2022-01-10 23:33:53 -0600

    Workout Modal Creation | Day One

---
+++ models/workout.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const WorkoutSchema = new mongoose.Schema({
+	name: String,
+  description: String,
+  exercises: Array,
+  repsAndSets: Array
+});
+
+const Workout = mongoose.model('Workout', WorkoutSchema);
+
+module.exports = Workout;
